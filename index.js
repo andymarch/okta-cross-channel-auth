18adeaa2b5f2358d32f9afee16a6458ebd1d9c1c
Andy March <mail@andymarch.co.uk>
2022-03-02 13:13:31 +0000

    Initial working example

---
+++ index.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const session = require("express-session");
+const ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
+const { v4: uuidv4 } = require('uuid');
+var cache = require('memory-cache');
+
+const app = express()
+
+var hbs = exphbs.create({
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        jwt: function (token){
+            var atob = require('atob');
+            if (token != null) {
+                var base64Url = token.split('.')[1];
+                var base64 = base64Url.replace('-', '+').replace('_', '/');
+                return JSON.stringify(JSON.parse(atob(base64)), undefined, '\t');
+            } else {
+                return "Invalid or empty token was parsed"
+            }
+        }
+    }
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use("/static", express.static("static"));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: true
+  }))
+
+let oidc = new ExpressOIDC({
+    issuer: process.env.OKTA_OAUTH2_ISSUER,
+    client_id: process.env.OKTA_OAUTH2_CLIENT_ID_WEB,
+    client_secret: process.env.OKTA_OAUTH2_CLIENT_SECRET_WEB,
+    appBaseUrl: process.env.BASE_URI,
+    scope: process.env.SCOPES
+});
+
+app.use(oidc.router);
+
+const router = express.Router();
+
+router.get("/", async function(req,res) {
+    res.render("index");
+})
+
+router.get("/verification", oidc.ensureAuthenticated(), async function(req,res) {
+    var cacheid = uuidv4()
+    cache.put(cacheid,req.userContext.tokens.access_token)
+    res.render("complete",{verificationLink:process.env.BASE_URI+'/verify?id='+cacheid});
+})
+
+router.get("/verify", oidc.ensureAuthenticated(), async function(req,res) {
+    var token = cache.get(req.query.id)
+    //handle cache misses
+    res.render("verify",{token: token});
+})
+
+app.use(router)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Cross Channel Auth started on '+PORT))
